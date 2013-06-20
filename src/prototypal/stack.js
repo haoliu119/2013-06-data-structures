749cae4a9ec881287f70a28073d739af7537bc5c
Hack Reactor Students <github@hackreactor.com>
2013-06-19 22:52:37 -0700

    Refactored Protopypal Stack to pass all tests

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,26 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var stack = Object.create(Stack.stackMethods);
+
+  stack._storage = {};
+
+  stack._size = 0; // Hint: set an initial value here
+  return stack;
+};
+
+Stack.stackMethods = {};
+
+Stack.stackMethods.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Stack.stackMethods.pop = function(){
+  this._size && this._size--;
+  var stackTop = this._storage[this._size];
+  delete this._storage[this._size];
+  return stackTop;
+};
+
+Stack.stackMethods.size = function(){
+  return this._size;
 };
